0cb41e8b9d1ba27fa124aa4cf0d6bf36c72f94c8
Fatemeh Javidi <fatemeh.javidi@stud.fh-campuswien.ac.at>
2022-10-14 21:20:19 +0200

    fertig!

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,7 +1,9 @@
 package at.ac.fhcampuswien;
 
 import java.sql.SQLOutput;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class App {
 
@@ -34,13 +36,25 @@ public class App {
     }
 
 
-
-        // input your solution here
     }
 
     //todo Task 2
     public void stairs(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+        int number = scanner.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid number!");
+        } else {
+            int counter = 1;
+            for (int x = 1; x <= number; x++) {
+                for (int y = 1; y <= x; y++) {
+                    System.out.print(counter + " ");
+                    counter++;
+                }
+                System.out.println();
+            }
+        }
     }
 
     //todo Task 3
@@ -60,18 +74,107 @@ public class App {
 
     //todo Task 4
     public void printRhombus(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("h: ");
+        int a = scanner.nextInt();
+        System.out.print("c: ");
+        char b = scanner.next().charAt(0);
+        if (a % 2 == 0) {
+            System.out.println("Invalid number!");
+        } else {
+            for (int x = 0; x < a / 2 + 1; x++) {
+                for (int y = 1; y < (a / 2 + 1) - x; y++) {
+                    System.out.print(" ");
+                }
+                char zeichen = (char) (b - x);
+                for (int j = 0; j <= 2 * x; j++) {
+                    System.out.print(zeichen);
+                    if (j >= x) {
+                        zeichen--;
+                    } else {
+                        zeichen++;
+                    }
+                }
+                System.out.println();
+            }
+            for (int y = 0; y < a / 2; y++) {
+                for (int x = 0; x <= y; x++) {
+                    System.out.print(" ");
+                }
+                char zeichen = (char) (b - a / 2 + y);
+                for (int x = y + 1; x < a - (y + 1); x++) {
+                    if (x < a / 2 + 1) {
+                        zeichen++;
+                    } else {
+                        zeichen--;
+                    }
+                    System.out.print(zeichen);
+                }
+                System.out.println();
+            }
+        }
     }
+
 
     //todo Task 5
     public void marks(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int anzahl = 0;
+        float anzahl2 = 0f;
+        int anzahl3 = 0;
+        while (true) {
+            System.out.print("Mark " + (anzahl + 1) + ": ");
+            int mark = scanner.nextInt();
+            if (mark > 5 || mark < 0) {
+                System.out.println("Invalid mark!");
+            } else if (mark == 0) {
+                break;
+            } else {
+                anzahl2 = anzahl2 + mark;
+                anzahl++;
+                if (mark == 5) {
+                    anzahl3 = anzahl3 + 1;
+                }
+            }
+        }
+        float average = 0;
+        if (anzahl > 0) {
+            average = anzahl2 / anzahl;
+        }
+        System.out.printf("Average: %.2f", average);
+        System.out.println();
+        System.out.println("Negative marks: " + anzahl3);
     }
 
     //todo Task 6
+
+    /* Task 6 taken from the Internet / https://javahungry.blogspot.com/2019/05/happy-number-program-in-java-with-example.html / last visit:
+    10.10.2022 */
     public void happyNumbers(){
-        // input your solution here
+        System.out.print("n: ");
+        // Input the number
+        int inputNumber = new Scanner(System.in).nextInt();
+        Boolean result = isHappyNumber(inputNumber);
+        if (result == true)
+            System.out.println("Happy number!");
+        else
+            System.out.println("Sad number!");
     }
+
+        public static boolean isHappyNumber(int number) {
+            Set<Integer> seenNumbers = new HashSet<Integer>();
+            while(seenNumbers.add(number)){
+                int digitsSum = 0;
+                while(number > 0) {
+                    digitsSum += Math.pow(number % 10 , 2);
+                    number /= 10;
+                }
+                number = digitsSum;
+                if(number == 1)
+                    return true;
+            }
+            return false;
+        }
 
     public static void main(String[] args){
         App exercise2 = new App();
